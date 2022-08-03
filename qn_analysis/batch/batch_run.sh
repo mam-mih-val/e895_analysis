843c67b4f313c179c4abe833e5b3125c188b682a
mam-mih-val <mam.mih.val@gmail.com>
2022-08-03 13:41:43 +0300

    All correction steps

--- qn_analysis/batch/batch_run.sh
+++ qn_analysis/batch/batch_run.sh
@@ -18,45 +18,45 @@ module add GCC-Toolchain/
 source /scratch1/mmamaev/bmn_environment.sh
 echo
 date
-#echo "Running conversion ..."
-#echo "/scratch1/mmamaev/e895_analysis/build/converter/convert -i $filelist -o output.root --output-tree atree -s $colliding_system -e $collision_energy"
-#
-#/scratch1/mmamaev/e895_analysis/build/converter/convert \
-#                                                        -i $filelist \
-#                                                        -o output.root \
-#                                                        --output-tree atree \
-#                                                        -s $colliding_system \
-#                                                        -e $collision_energy
-#
-#find . -name "output*.root" > list.txt
+echo "Running conversion ..."
+echo "/scratch1/mmamaev/e895_analysis/build/converter/convert -i $filelist -o output.root --output-tree atree -s $colliding_system -e $collision_energy"
+
+/scratch1/mmamaev/e895_analysis/build/converter/convert \
+                                                        -i $filelist \
+                                                        -o output.root \
+                                                        --output-tree atree \
+                                                        -s $colliding_system \
+                                                        -e $collision_energy
+
+find . -name "output*.root" > list.txt
+echo; date
+#echo "Running QA step ..."
+#input_list=\"list.txt\"
+#output_qa=\"qa.root\"
+#root -q "/scratch1/mmamaev/mcpico_to_analysis_tree/macro/run_qa.cc($input_list, $output_qa)"
 #echo; date
-##echo "Running QA step ..."
-##input_list=\"list.txt\"
-##output_qa=\"qa.root\"
-##root -q "/scratch1/mmamaev/mcpico_to_analysis_tree/macro/run_qa.cc($input_list, $output_qa)"
-##echo; date
-#echo "Running correction ..."
-#/scratch1/mmamaev/QnAnalysis/build/src/QnAnalysisCorrect/QnAnalysisCorrect \
-#                                                                          -i list.txt \
-#                                                                          -t atree \
-#                                                                          -o correction_out.root \
-#                                                                          --yaml-config-file /scratch1/mmamaev/e895_analysis/qn_analysis/setups/e895_corrections.yml \
-#                                                                          --yaml-config-name E895
-#mv correction_out.root correction_in.root
-#/scratch1/mmamaev/QnAnalysis/build/src/QnAnalysisCorrect/QnAnalysisCorrect \
-#                                                                          -i list.txt \
-#                                                                          -t atree \
-#                                                                          -o correction_out.root \
-#                                                                          --yaml-config-file /scratch1/mmamaev/e895_analysis/qn_analysis/setups/e895_corrections.yml \
-#                                                                          --yaml-config-name E895
-#mv correction_out.root correction_in.root
-#/scratch1/mmamaev/QnAnalysis/build/src/QnAnalysisCorrect/QnAnalysisCorrect \
-#                                                                          -i list.txt \
-#                                                                          -t atree \
-#                                                                          -o correction_out.root \
-#                                                                          --yaml-config-file /scratch1/mmamaev/e895_analysis/qn_analysis/setups/e895_corrections.yml \
-#                                                                          --yaml-config-name E895
-#echo; date
+echo "Running correction ..."
+/scratch1/mmamaev/QnAnalysis/build/src/QnAnalysisCorrect/QnAnalysisCorrect \
+                                                                          -i list.txt \
+                                                                          -t atree \
+                                                                          -o correction_out.root \
+                                                                          --yaml-config-file /scratch1/mmamaev/e895_analysis/qn_analysis/setups/e895_corrections.yml \
+                                                                          --yaml-config-name E895
+mv correction_out.root correction_in.root
+/scratch1/mmamaev/QnAnalysis/build/src/QnAnalysisCorrect/QnAnalysisCorrect \
+                                                                          -i list.txt \
+                                                                          -t atree \
+                                                                          -o correction_out.root \
+                                                                          --yaml-config-file /scratch1/mmamaev/e895_analysis/qn_analysis/setups/e895_corrections.yml \
+                                                                          --yaml-config-name E895
+mv correction_out.root correction_in.root
+/scratch1/mmamaev/QnAnalysis/build/src/QnAnalysisCorrect/QnAnalysisCorrect \
+                                                                          -i list.txt \
+                                                                          -t atree \
+                                                                          -o correction_out.root \
+                                                                          --yaml-config-file /scratch1/mmamaev/e895_analysis/qn_analysis/setups/e895_corrections.yml \
+                                                                          --yaml-config-name E895
+echo; date
 echo "Running correlation ..."
 /scratch1/mmamaev/QnAnalysis/build/src/QnAnalysisCorrelate/QnAnalysisCorrelate \
                                                                               --input-file correction_out.root \
